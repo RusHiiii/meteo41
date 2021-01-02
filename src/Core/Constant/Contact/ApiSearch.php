f1af475ff1d16ba39e972e6b9cccbadffaec3fc8
RusHiiii <fdamiens@itnetwork.fr>
2021-01-02 15:32:34 +0100

    Add contact paginated result and PHPCS

---
+++ src/Core/Constant/Contact/ApiSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Core\Constant\Contact;
+
+
+class ApiSearch
+{
+    const CONTACT_ORDER_BY_DESC = 'DESC';
+    const CONTACT_ORDER_BY_ASC = 'ASC';
+
+    const CONTACT_SEARCH_BY_EMAIL = 'email';
+}
---
+++ src/Entity/Core/ViewModels/Contact/ContactSearchView.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Entity\Core\ViewModels\Contact;
+
+
+class ContactSearchView
+{
+    private int $numberOfResult;
+
+    private array $contacts;
+
+    /**
+     * ContactSearchView constructor.
+     * @param int $numberOfResult
+     * @param array $contacts
+     */
+    public function __construct(int $numberOfResult, array $contacts)
+    {
+        $this->numberOfResult = $numberOfResult;
+        $this->contacts = $contacts;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfResult(): int
+    {
+        return $this->numberOfResult;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContacts(): array
+    {
+        return $this->contacts;
+    }
+}
