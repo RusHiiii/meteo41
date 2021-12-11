3a4c5c28b6d03922afdf712bc546d75fcb267117
RusHiiii <fdamiens@itnetwork.fr>
2021-12-11 17:06:38 +0100

    Add range gauge

--- assets/js/front/home/components/gauge/RainDaily.js
+++ assets/js/front/home/components/gauge/RainDaily.js
@@ -12,14 +12,15 @@ let grainDaily = null;
 function initGauge(canvas, value, unit) {
   grainDaily = new RainGauge(canvas, {
     minValue: 0,
-    maxValue: 25,
-    majorTicks: ['0', '5', '10', '15', '20', '25'],
+    maxValue: 30,
+    majorTicks: ['0', '5', '10', '15', '20', '25', '30'],
     highlights: [
-      { from: 0, to: 5, color: '#DBEFF5' },
-      { from: 5, to: 10, color: '#B6DFEB' },
-      { from: 10, to: 15, color: '#92CFE1' },
-      { from: 15, to: 20, color: '#6DBFD7' },
-      { from: 20, to: 25, color: '#49AFCD' },
+      { from: 0, to: 5, color: '#D6ECF4' },
+      { from: 5, to: 10, color: '#ADD9E9' },
+      { from: 10, to: 15, color: '#85c6df' },
+      { from: 15, to: 20, color: '#5cb3d4' },
+      { from: 20, to: 25, color: '#33a0c9' },
+      { from: 25, to: 30, color: '#0d7499' },
     ],
   });
 
