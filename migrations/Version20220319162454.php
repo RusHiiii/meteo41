659ba765bdb3d952ed8231f072ef121377026e0d
RusHiiii <fdamiens@itnetwork.fr>
2022-03-19 17:25:04 +0100

    wip

---
+++ migrations/Version20220319162454.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220319162454 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE weather_data ADD date DATE DEFAULT NULL');
+        $this->addSql('CREATE INDEX weather_data_date ON weather_data (date)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX weather_data_date');
+        $this->addSql('ALTER TABLE weather_data DROP date');
+    }
+}
