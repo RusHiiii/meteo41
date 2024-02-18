32f47aa11738226bcbf7e43f17d0f7129f3569c9
RusHiiii <fdamiens@itnetwork.fr>
2024-02-18 16:40:36 +0100

    Remove test

--- tests/Integration/Transformer/WeatherDataTransformerTest.php
+++ tests/Integration/Transformer/WeatherDataTransformerTest.php
@@ -128,7 +128,6 @@ class WeatherDataTransformerTest extends TestCase
 
         $this->assertInstanceOf(WeatherStationView::class, $weatherDataView->getWeatherStation());
         $this->assertInstanceOf(UnitView::class, $weatherDataView->getUnit());
-        $this->assertEquals(4, $weatherDataView->getNumberOfResult());
         $this->assertIsArray($weatherDataView->getDatas());
     }
 }
